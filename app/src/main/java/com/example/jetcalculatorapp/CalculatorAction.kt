22beb4b46c9b3e881f19be6e6e91c833bc286520
rizkysiregar <regarrezky@gmail.com>
2023-04-16 14:00:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetcalculatorapp/CalculatorAction.kt
@@ -0,0 +1,13 @@
+package com.example.jetcalculatorapp
+
+sealed class CalculatorAction{
+    data class Number(val number: Int): CalculatorAction()
+
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+}
